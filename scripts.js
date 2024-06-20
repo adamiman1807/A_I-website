30d3a7c13a56f9da577c8596fffab5283babbd5a
adamiman1807 <adamiman1807@gmail.com>
2024-06-21 00:46:17 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('myButton').addEventListener('click',function() {
+    var flowerContainer = document.getElementById('flowerContainer');
+    flowerContainer.classList.toggle('visible');
+    flowerContainer.classList.toggle('hidden');
+    alert('YOU ClICKED IT OMGGGG THANKSSSSS !!!!!!!!');
+});
